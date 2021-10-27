df1983743dcc68f96139be143593568b6efb93a9
Galatia <ptrivedius@aol.com>
2021-10-27 17:24:02 -0400

    pushing current code

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js"); 
 
+router.get("/api/workout", ({ body }, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
 router.post("/api/workout", ({ body }, res) => {
   Workout.create(body)
     .then(dbWorkout  => {
